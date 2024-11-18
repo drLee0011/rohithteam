231c5e7d2e1aa7f71e849465a450122e164de37c
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-18 12:37:47 +0000

    variable.tf

--- variable.tf
+++ variable.tf
@@ -13,5 +13,5 @@ variable "ami_id" {
 variable "key_pair_name" {
   description = "Name of the key pair for SSH access"
   type        = string
-  default     = "ca1.pem"  # Replace with your key pair name
+  default     = "ca1"  # Replace with your key pair name
 }
