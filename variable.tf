847aa11dc7c9935bccd339945a0a86909c0b7165
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-18 09:21:48 +0000

    variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-north-1" # Ireland region
+  default     = "eu-north-1"  # Change this if needed to match your region
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
-  default     = "ami-08eb150f611ca277f" # Amazon Linux 2 AMI ID for eu-west-1
+  default     = "ami-08eb150f611ca277f"  # Ensure this AMI ID is for your region
 }
 
-# variable "key_pair_name" {
-#   description = "Name of the key pair for SSH access"
-#   type        = string
-#   default     = "terraform"
-# }
+variable "key_pair_name" {
+  description = "Name of the key pair for SSH access"
+  type        = string
+  default     = "CA_network"  # Replace with your key pair name
+}
