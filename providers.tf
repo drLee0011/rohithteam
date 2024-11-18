be0190aecaabee9c0fbaf85dbf86c507639792c8
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-18 10:51:32 +0000

    providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = var.aws_region
-  aws_access_key_id = NEW_AWS_ACCESS_KEY_ID
-  aws_secret_access_key = NEW_AWS_SECRET_ACCESS_KEY
-
+  access_key = "AKIAQSOI4QNHH6GFMIUQ"
+  secret_key = "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"
 }
