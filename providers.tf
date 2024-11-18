ca2d28915d5cf9f89e3bbb16a7f5503901602f85
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-18 11:17:02 +0000

    providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIAQSOI4QNHH6GFMIUQ"
-  secret_key = "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"
+  access_key = "AKIAXWHDMBIZE6DSBEX7"
+  secret_key = "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"
 }
