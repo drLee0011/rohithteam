9d33d6662cfa57171eae23924cb11d4102fbb69c
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-18 10:21:02 +0000

    variable.tf

--- variable.tf
+++ variable.tf
@@ -13,5 +13,5 @@ variable "ami_id" {
 variable "key_pair_name" {
   description = "Name of the key pair for SSH access"
   type        = string
-  default     = "terraform"  # Replace with your key pair name
+  default     = "CA_1"  # Replace with your key pair name
 }
